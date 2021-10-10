dedda332a0863cc85e5b1de57fa50bc66975c03e
ndTh4ng <thangxacam@gmail.com>
2021-10-10 15:53:06 +0700

    Initial Commit

---
+++ src/data.js
@@ -0,0 +1,41 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://o.remove.bg/downloads/1dd4a128-0495-494b-be7d-1fc8369cb40e/1-removebg-preview.png",
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd"
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/DG69bQ4/2.png",
+        title: "AUTUMN COLLECTION",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fcf1ed"
+    },
+    {
+        id: 3,
+        img: "https://o.remove.bg/downloads/b871da5c-3e19-4ca9-9af6-887a0ebcb199/2-removebg-preview.png",
+        title: "LOUNGEWEAR LOVE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fbf0f4"
+    }
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://media1.popsugar-assets.com/files/thumbor/ReJjr9EwiOcIZBduzoe4lz8EKx4/0x187:1080x1267/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2019/05/09/857/n/1922564/f26ae3fc5cd480f14655c2.04038004_/i/Ways-Wear-White-T-Shirt.jpg",
+        title: "SHIRT STYLE!",
+    },
+    {
+        id: 2,
+        img: "https://cdn.shopify.com/s/files/1/0687/7613/products/8_99ff1726-ff25-46c7-88f5-e99ad74c179d_1200x.jpg?v=1615933147",
+        title: "LOUNGWEAR LOVE",
+    },
+    {
+        id: 3,
+        img: "https://cdn.shopify.com/s/files/1/0883/7678/products/soot_and_ty_black_beige_5_unisex_reversible_tropical_bomber_jacket_800x.jpg?v=1613455167",
+        title: "LIGHT JACKETS",
+    },
+];
